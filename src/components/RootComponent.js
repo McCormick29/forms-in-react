211835a55060a35ec7ccb3b47fd51cf0ef1db12e
Levi McCormick <levimccormick34@gmail.com>
2021-08-23 10:54:09 -0600

    Added Yup Schema

--- src/components/RootComponent.js
+++ src/components/RootComponent.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
-import SignUpComponent from "./SignUpComponent"
+import SignUpComponent from "./SignUpYup"
 
 class RootComponent extends React.Component {
 
